d33fe1ef7ec6dda606f7bc23205e44cdf9a66299
ShaunakPerni <134259584+ShaunakPerni@users.noreply.github.com>
2023-10-30 19:15:18 +0530

    create table object header

---
+++ bcppTableObjects.h
@@ -0,0 +1,12 @@
+#include <vector>
+template <typename RowType> class AccountingTable{
+    private:
+    std::vector<RowType> tableColumns;
+    public:
+    AccountingTable(RowType[] inputRows){
+        for(RowType a : inputRows){
+
+        }
+    }
+};
+
